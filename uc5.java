167b7ffdb41734f6b6e7aecc08479a551e60e859
Yesudhas M <ynsameera@gmail.com>
2026-03-18 10:03:54 +0530

    uc5

---
+++ uc5.java
@@ -0,0 +1,14 @@
+public class uc5 {
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            System.out.print("Hello ");
+            for (String name : args) {
+                System.out.print(name + " ");
+            }
+
+        } else {
+            System.out.println("Hello World");
+        }
+    }
+}
